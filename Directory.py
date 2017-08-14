d5457ebc61101d222fa71876acfcf3a14f58259d
Glenn <glenn.story@gmail.com>
2017-08-13 18:28:58 -0700

    Remove tabs from python file

--- Directory.py
+++ Directory.py
@@ -28,18 +28,18 @@
 import csv
 
 # defaults
-rowNum = 0	# row of input
+rowNum = 0      # row of input
 kidCount = 0
 # Friggin Python doesn't have Java style 'for'??
 blocks = ['','','','','','','','','','','','','','','','','','','','','','']
 kids = ['','','','','','','','','','','','']
 redirects = ['']
-redirects[:] = []		# add to this as new redirects are found
+redirects[:] = []               # add to this as new redirects are found
 redirCount = 0
 currAcct = ''
 acct1stName = ''
 acctLastName = ''
-acctPName = ''		# what will be printed in the directory
+acctPName = ''          # what will be printed in the directory
 acctType = ''
 acctPType = ''
 addr = ''
@@ -50,21 +50,21 @@ blockNum = 0
 inDir = False
 
 # Change these as necessary!!
-currYear = 2017		# one step below hard-coding
+currYear = 2017         # one step below hard-coding
 directory = "C://bruces/temp/GM DB/"
 
 gmOut = open('gmOut.txt', mode='w')
 with open('customexport.csv') as csvfile:
     rdr = csv.DictReader(csvfile)
     for row in rdr:
-        rowNum=rowNum+1	# no friggin increment?!
-        if (row['Account Status'] != 'Active') : 	# or row['Member Status'] != 'Active'): 
-             continue	# ignore entry if hidden
+        rowNum=rowNum+1 # no friggin increment?!
+        if (row['Account Status'] != 'Active') :        # or row['Member Status'] != 'Active'): 
+             continue   # ignore entry if hidden
         # If new account
         if (row['Email'] != currAcct):
             # First save last Acct info (if valid)
             # Actually first convert acctType to shortened value and fix for Greenmeadow Swim Team
-            acctPType = 'WHOOPS! ' + acctType	# default - hopefully always overridden
+            acctPType = 'WHOOPS! ' + acctType   # default - hopefully always overridden
             if (acctType == 'Greenmeadow Swim Team') :
                 if (blockNum != 0) : acctPType = 'AS'
                 else : acctPType = 'RS'
@@ -85,7 +85,7 @@ with open('customexport.csv') as csvfile:
                     for kid in kids: kidlist = kidlist + kid + " "
                     gmOut.write(kidlist + ' \t' + cityZip + ' \t' + email + "\r")
                 else : gmOut.write('\t\t\t' + cityZip + ' \t' + email + "\r")
-                gmOut.write("\r")	# just a separator line
+                gmOut.write("\r")       # just a separator line
                 if (blockNum != 0):
                     if (kidCount == 0) : 
                         # Save block info (all but email?)
@@ -94,14 +94,14 @@ with open('customexport.csv') as csvfile:
                         blocks[blockNum-1] = blocks[blockNum-1] + addr + ' \t' + acctPName + " \t" + phone + " \t" + acctPType + "\r\t\t" + kidlist + '\r'
                 #
             else: 
-                while redirCount > 0 :		 # gotta be a better way to do this, but...
+                while redirCount > 0 :           # gotta be a better way to do this, but...
                     del redirects[-1]
                     redirCount = redirCount-1
             # Default new Acct params
-            inDir = False		# default case
-            redirCount = 0		# reset to no redirects for new acct
-            member = False		# ditto (guilty until proven innocent!)
-            kidCount = 0		# reset to no kids for new account
+            inDir = False               # default case
+            redirCount = 0              # reset to no redirects for new acct
+            member = False              # ditto (guilty until proven innocent!)
+            kidCount = 0                # reset to no kids for new account
             kids = ['','','','','','','','','','','','']
             currAcct = row['Email']
             acct1stName = row['Acct. First Name'].strip()
@@ -115,7 +115,7 @@ with open('customexport.csv') as csvfile:
             else:  email = ' - '
             # get Block Number into blockNum - if it's valid (assuming block# applied to all residents)
             btemp = row['Residents Block Number']
-            blockNum = 0		# default
+            blockNum = 0                # default
             if (btemp.isdecimal()):
                 blkInt = 0
                 blkInt = int(btemp)
@@ -153,7 +153,7 @@ with open('customexport.csv') as csvfile:
         if (memLastName.lower() != acctLastName.lower() and (yearBlank or currYear-yob >= 30)):
             #add to redirect list
             redirects.append(memLastName + ', ' + mem1stName + ' -> ' + acctLastName + ', ' + acct1stName)
-            redirCount = redirCount+1	# to roll back if account invalid
+            redirCount = redirCount+1   # to roll back if account invalid
             # create new acct name as combo of existing parent + other named one
             if not '&' in acct1stName:
                  acctPName = acctPName + " & " + mem1stName + ' ' + memLastName
@@ -166,7 +166,7 @@ with open('customexport.csv') as csvfile:
         # now check if parent by 1st name match
         elif ((mem1stName.lower() in acct1stName.lower()) and (yearBlank or currYear - yob >= 30)):
             # probably parent, unless child name matches/part of parent and year blank - sigh
-            junk = 0		# no action, but that's not allowed in python??
+            junk = 0            # no action, but that's not allowed in python??
         # else check for kid - almost certainly if yob<30, else (dob blank) guessing
         elif (yearBlank or (currYear - yob) < 30):
             # kid (hopefully) so add to record with birth month/year
@@ -176,7 +176,7 @@ with open('customexport.csv') as csvfile:
         elif (currYear - yob >= 30) and not '&' in acct1stName:
             # add (hopefully) parent to acctName
             acctPName = acctPName + " & " +mem1stName
-        #if (acctLastName == "Brodt") : break	# for testing purposes - short printout
+        #if (acctLastName == "Brodt") : break   # for testing purposes - short printout
     # end of (input) rows
     gmOut.close()
     with open('blocksOut.txt', mode='w') as blkout:
